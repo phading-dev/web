ba0faf3484700afe5924c18dfc1217fd139e3769
Yuxuan Xie <teststaybaka@gmail.com>
2023-10-01 00:13:13 +0000

    Migrated UpdateAvatarPage.

---
+++ show_page/consumer_page/account_page/profile_page/update_avatar_page/body.ts
@@ -0,0 +1,276 @@
+import EventEmitter = require("events");
+import {
+  FilledBlockingButton,
+  OutlineBlockingButton,
+} from "../../../../../common/blocking_button";
+import { SCHEME } from "../../../../../common/color_scheme";
+import { ImageCropper } from "../../../../../common/image_cropper/body";
+import { LOCALIZED_TEXT } from "../../../../../common/locales/localized_text";
+import { MenuItem } from "../../../../../common/menu_item/body";
+import { createBackMenuItem } from "../../../../../common/menu_item/factory";
+import {
+  MEDIUM_CARD_STYLE,
+  PAGE_STYLE,
+} from "../../../../../common/page_style";
+import { USER_SERVICE_CLIENT } from "../../../../../common/web_service_client";
+import { uploadAvatar } from "@phading/user_service_interface/client_requests";
+import { E } from "@selfage/element/factory";
+import { Ref, assign } from "@selfage/ref";
+import { WebServiceClient } from "@selfage/web_service_client";
+
+export interface UpdateAvatarPage {
+  on(event: "back", listener: () => void): this;
+  on(event: "imageLoaded", listener: () => void): this;
+  on(event: "updateError", listener: () => void): this;
+  on(event: "updated", listener: () => void): this;
+}
+
+export class UpdateAvatarPage extends EventEmitter {
+  private static LARGE_IMAGE_LENGTH = 160;
+  private static SMALL_IMAGE_LENGTH = 50;
+
+  public body: HTMLDivElement;
+  public backMenuBody: HTMLDivElement;
+  // Visible for testing
+  public backMenuItem: MenuItem;
+  public chooseFileButton: OutlineBlockingButton;
+  public uploadButton: FilledBlockingButton;
+  public imageCropper: ImageCropper;
+  private loadErrorText: HTMLDivElement;
+  private previewLargeCanvas: HTMLCanvasElement;
+  private previewSmallCanvas: HTMLCanvasElement;
+  private uploadStatusText: HTMLDivElement;
+
+  public constructor(private userServiceClient: WebServiceClient) {
+    super();
+    let chooseFileButtonRef = new Ref<OutlineBlockingButton>();
+    let loadErrorTextRef = new Ref<HTMLDivElement>();
+    let imageCropperRef = new Ref<ImageCropper>();
+    let previewLargeCanvasRef = new Ref<HTMLCanvasElement>();
+    let previewSmallCanvasRef = new Ref<HTMLCanvasElement>();
+    let uploadButtonRef = new Ref<FilledBlockingButton>();
+    let uploadStatusTextRef = new Ref<HTMLDivElement>();
+    this.body = E.div(
+      {
+        class: "update-avatar",
+        style: PAGE_STYLE,
+      },
+      E.div(
+        {
+          class: "update-avatar-card",
+          style: `${MEDIUM_CARD_STYLE} display: flex; flex-flow: column nowrap; gap: 2rem; align-items: center;`,
+        },
+        assign(
+          chooseFileButtonRef,
+          OutlineBlockingButton.create(
+            "",
+            E.text(LOCALIZED_TEXT.chooseAvatarLabel)
+          ).enable()
+        ).body,
+        E.divRef(
+          loadErrorTextRef,
+          {
+            class: "update-avatar-image-load-error",
+            style: `visibility: hidden; font-size: 1.4rem; color: ${SCHEME.error0};`,
+          },
+          E.text("1")
+        ),
+        E.div(
+          {
+            class: "update-avatar-canvas-wrapper",
+            style: `width: 46rem; height: 46rem;`,
+          },
+          assign(imageCropperRef, ImageCropper.create()).body
+        ),
+        E.div(
+          {
+            class: "update-avatar-preview-label",
+            style: `font-size: 1.4rem; color: ${SCHEME.neutral0};`,
+          },
+          E.text(LOCALIZED_TEXT.previewAvatarLabel)
+        ),
+        E.div(
+          {
+            class: "update-avatar-preview-line",
+            style: `display: flex; flex-flow: row nowrap; width: 100%; justify-content: center; align-items: flex-end; gap: 5rem;`,
+          },
+          E.div(
+            {
+              class: "update-avatar-preview-large-container",
+              style: `display: flex; flex-flow: column nowrap; align-items: center; gap: 2rem;`,
+            },
+            E.div(
+              {
+                class: "update-avatar-preview-large-cap",
+                style: `position: relative; width: ${UpdateAvatarPage.LARGE_IMAGE_LENGTH}px; height: ${UpdateAvatarPage.LARGE_IMAGE_LENGTH}px; border-radius: ${UpdateAvatarPage.LARGE_IMAGE_LENGTH}px; border: .1rem solid ${SCHEME.neutral1}; overflow: hidden;`,
+              },
+              E.canvasRef(previewLargeCanvasRef, {
+                class: "update-avatar-preview-large-canvas",
+                style: `position: absolute;`,
+              })
+            ),
+            E.div(
+              {
+                class: "update-avatar-preview-large-label",
+                style: `font-size: 1.4rem; color: ${SCHEME.neutral0};`,
+              },
+              E.text("160 x 160")
+            )
+          ),
+          E.div(
+            {
+              class: "update-avatar-preview-large-container",
+              style: `display: flex; flex-flow: column nowrap; align-items: center; gap: 2rem;`,
+            },
+            E.div(
+              {
+                class: "update-avatar-preview-small-cap",
+                style: `position: relative; width: ${UpdateAvatarPage.SMALL_IMAGE_LENGTH}px; height: ${UpdateAvatarPage.SMALL_IMAGE_LENGTH}px; border-radius: ${UpdateAvatarPage.SMALL_IMAGE_LENGTH}px; border: .1rem solid ${SCHEME.neutral1}; overflow: hidden;`,
+              },
+              E.canvasRef(previewSmallCanvasRef, {
+                class: "change-vatar-preview-small-canvas",
+                style: `position: absolute;`,
+              })
+            ),
+            E.div(
+              {
+                class: "update-avatar-preview-small-label",
+                style: `font-size: 1.4rem; color: ${SCHEME.neutral0};`,
+              },
+              E.text("50 x 50")
+            )
+          )
+        ),
+        assign(
+          uploadButtonRef,
+          FilledBlockingButton.create(
+            "",
+            E.text(LOCALIZED_TEXT.uploadAvatarLabel)
+          ).disable()
+        ).body,
+        E.divRef(
+          uploadStatusTextRef,
+          {
+            class: "update-avatar-upload-status-text",
+            style: `visibility: hidden; font-size: 1.4rem; color: ${SCHEME.error0};`,
+          },
+          E.text("1")
+        )
+      )
+    );
+    this.chooseFileButton = chooseFileButtonRef.val;
+    this.loadErrorText = loadErrorTextRef.val;
+    this.imageCropper = imageCropperRef.val;
+    this.previewLargeCanvas = previewLargeCanvasRef.val;
+    this.previewSmallCanvas = previewSmallCanvasRef.val;
+    this.uploadButton = uploadButtonRef.val;
+    this.uploadStatusText = uploadStatusTextRef.val;
+
+    this.backMenuItem = createBackMenuItem();
+    this.backMenuBody = this.backMenuItem.body;
+
+    this.backMenuItem.on("action", () => this.emit("back"));
+    this.chooseFileButton.on("action", () => this.chooseFile());
+    this.imageCropper.on("change", () => this.preview());
+    this.uploadButton.on("action", () => this.uploadAvatar());
+    this.uploadButton.on("postAction", (error) => this.postUploadAvatar(error));
+  }
+
+  public static create(): UpdateAvatarPage {
+    return new UpdateAvatarPage(USER_SERVICE_CLIENT);
+  }
+
+  private async chooseFile(): Promise<void> {
+    await new Promise<void>((resolve) => {
+      let fileInput = E.input({ type: "file" });
+      fileInput.addEventListener("input", async () => {
+        await this.load(fileInput.files);
+        resolve();
+      });
+      fileInput.click();
+    });
+  }
+
+  private async load(files: FileList): Promise<void> {
+    this.loadErrorText.style.visibility = "hidden";
+    try {
+      await this.imageCropper.load(files[0]);
+    } catch (e) {
+      this.loadErrorText.textContent = LOCALIZED_TEXT.loadImageError;
+      this.loadErrorText.style.visibility = "visible";
+      console.error(e);
+      this.emit("imageLoaded");
+      return;
+    }
+
+    this.previewLargeCanvas.width = this.imageCropper.canvas.width;
+    this.previewLargeCanvas.height = this.imageCropper.canvas.height;
+    this.previewLargeCanvas
+      .getContext("2d")
+      .drawImage(this.imageCropper.canvas, 0, 0);
+    this.previewSmallCanvas.width = this.imageCropper.canvas.width;
+    this.previewSmallCanvas.height = this.imageCropper.canvas.height;
+    this.previewSmallCanvas
+      .getContext("2d")
+      .drawImage(this.imageCropper.canvas, 0, 0);
+    this.uploadButton.enable();
+    this.emit("imageLoaded");
+  }
+
+  private preview(): void {
+    this.previewLargeCanvas.style.width = `${
+      (this.imageCropper.canvas.width / this.imageCropper.sWidth) *
+      UpdateAvatarPage.LARGE_IMAGE_LENGTH
+    }px`;
+    this.previewLargeCanvas.style.height = `${
+      (this.imageCropper.canvas.height / this.imageCropper.sHeight) *
+      UpdateAvatarPage.LARGE_IMAGE_LENGTH
+    }px`;
+    this.previewLargeCanvas.style.left = `-${
+      (this.imageCropper.sx / this.imageCropper.sWidth) *
+      UpdateAvatarPage.LARGE_IMAGE_LENGTH
+    }px`;
+    this.previewLargeCanvas.style.top = `-${
+      (this.imageCropper.sy / this.imageCropper.sHeight) *
+      UpdateAvatarPage.LARGE_IMAGE_LENGTH
+    }px`;
+    this.previewSmallCanvas.style.width = `${
+      (this.imageCropper.canvas.width / this.imageCropper.sWidth) *
+      UpdateAvatarPage.SMALL_IMAGE_LENGTH
+    }px`;
+    this.previewSmallCanvas.style.height = `${
+      (this.imageCropper.canvas.height / this.imageCropper.sHeight) *
+      UpdateAvatarPage.SMALL_IMAGE_LENGTH
+    }px`;
+    this.previewSmallCanvas.style.left = `-${
+      (this.imageCropper.sx / this.imageCropper.sWidth) *
+      UpdateAvatarPage.SMALL_IMAGE_LENGTH
+    }px`;
+    this.previewSmallCanvas.style.top = `-${
+      (this.imageCropper.sy / this.imageCropper.sHeight) *
+      UpdateAvatarPage.SMALL_IMAGE_LENGTH
+    }px`;
+  }
+
+  private async uploadAvatar(): Promise<void> {
+    this.uploadStatusText.style.visibility = "hidden";
+    let blob = await this.imageCropper.export();
+    await uploadAvatar(this.userServiceClient, blob);
+  }
+
+  private postUploadAvatar(error?: Error): void {
+    if (error) {
+      console.error(error);
+      this.uploadStatusText.textContent = LOCALIZED_TEXT.uploadAvatarError;
+      this.uploadStatusText.style.visibility = "visible";
+      this.emit("updateError");
+    } else {
+      this.emit("updated");
+    }
+  }
+
+  public remove(): void {
+    this.backMenuItem.remove();
+    this.body.remove();
+  }
+}
---
+++ show_page/consumer_page/account_page/profile_page/update_avatar_page/body_test.ts
@@ -0,0 +1,213 @@
+import nonImageFile = require("./test_data/non_image.bin");
+import wideImage = require("./test_data/wide.jpeg");
+import path = require("path");
+import { UpdateAvatarPage } from "./body";
+import { UploadAvatarResponse } from "@phading/user_service_interface/interface";
+import { E } from "@selfage/element/factory";
+import {
+  setViewport,
+  supplyFiles,
+  writeFile,
+} from "@selfage/puppeteer_test_executor_api";
+import { TEST_RUNNER, TestCase } from "@selfage/puppeteer_test_runner";
+import {
+  asyncAssertImage,
+  asyncAssertScreenshot,
+} from "@selfage/screenshot_test_matcher";
+import { assertThat, eq } from "@selfage/test_matcher";
+import { WebServiceClient } from "@selfage/web_service_client";
+import "../../../../../common/normalize_body";
+
+let menuContainer: HTMLDivElement;
+
+TEST_RUNNER.run({
+  name: "UpdateAvatarPageTest",
+  environment: {
+    setUp: () => {
+      menuContainer = E.div({
+        style: `position: fixed; left: 0; top: 0;`,
+      });
+      document.body.appendChild(menuContainer);
+    },
+    tearDown: () => {
+      menuContainer.remove();
+    },
+  },
+  cases: [
+    new (class implements TestCase {
+      public name = "Default_Load_Resize";
+      private cut: UpdateAvatarPage;
+      public async execute() {
+        // Prepare
+        await setViewport(1000, 800);
+
+        // Execute
+        this.cut = new UpdateAvatarPage(undefined);
+        document.body.append(this.cut.body);
+        menuContainer.append(this.cut.backMenuBody);
+
+        // Verify
+        await asyncAssertScreenshot(
+          path.join(__dirname, "/update_avatar_page_default.png"),
+          path.join(__dirname, "/golden/update_avatar_page_default.png"),
+          path.join(__dirname, "/update_avatar_page_default_diff.png")
+        );
+
+        // Execute
+        window.scrollTo(0, document.body.scrollHeight);
+
+        // Verify
+        await asyncAssertScreenshot(
+          path.join(__dirname, "/update_avatar_page_scroll_to_bottom.png"),
+          path.join(
+            __dirname,
+            "/golden/update_avatar_page_scroll_to_bottom.png"
+          ),
+          path.join(__dirname, "/update_avatar_page_scroll_to_bottom_diff.png")
+        );
+
+        // Execute
+        supplyFiles(() => this.cut.chooseFileButton.click(), wideImage);
+        await new Promise<void>((resolve) =>
+          this.cut.once("imageLoaded", resolve)
+        );
+
+        // Verify
+        await asyncAssertScreenshot(
+          path.join(__dirname, "/update_avatar_page_loaded.png"),
+          path.join(__dirname, "/golden/update_avatar_page_loaded.png"),
+          path.join(__dirname, "/update_avatar_page_loaded_diff.png")
+        );
+
+        // Execute
+        this.cut.imageCropper.resizePointTopLeft.dispatchEvent(
+          new MouseEvent("mousedown", {
+            clientX: 10,
+            clientY: 10,
+            bubbles: true,
+          })
+        );
+
+        // Verify
+        await asyncAssertScreenshot(
+          path.join(__dirname, "/update_avatar_page_moved_resized.png"),
+          path.join(__dirname, "/golden/update_avatar_page_moved_resized.png"),
+          path.join(__dirname, "/update_avatar_page_moved_resized_diff.png")
+        );
+      }
+      public tearDown() {
+        this.cut.remove();
+      }
+    })(),
+    new (class implements TestCase {
+      public name = "LoadError";
+      private cut: UpdateAvatarPage;
+      public async execute() {
+        // Prepare
+        await setViewport(1000, 900);
+        this.cut = new UpdateAvatarPage(undefined);
+        document.body.append(this.cut.body);
+        menuContainer.append(this.cut.backMenuBody);
+
+        // Execute
+        supplyFiles(() => this.cut.chooseFileButton.click(), nonImageFile);
+        await new Promise<void>((resolve) =>
+          this.cut.once("imageLoaded", resolve)
+        );
+
+        // Verify
+        await asyncAssertScreenshot(
+          path.join(__dirname, "/update_avatar_page_load_error.png"),
+          path.join(__dirname, "/golden/update_avatar_page_load_error.png"),
+          path.join(__dirname, "/update_avatar_page_load_error_diff.png")
+        );
+      }
+      public tearDown() {
+        this.cut.remove();
+      }
+    })(),
+    new (class implements TestCase {
+      public name = "Upload";
+      private cut: UpdateAvatarPage;
+      public async execute() {
+        // Prepare
+        let clientMock = new (class extends WebServiceClient {
+          public constructor() {
+            super(undefined, undefined);
+          }
+        })();
+        await setViewport(1000, 1200);
+        this.cut = new UpdateAvatarPage(clientMock);
+        document.body.append(this.cut.body);
+        menuContainer.append(this.cut.backMenuBody);
+
+        supplyFiles(() => this.cut.chooseFileButton.click(), wideImage);
+        await new Promise<void>((resolve) =>
+          this.cut.once("imageLoaded", resolve)
+        );
+
+        clientMock.send = () => {
+          throw new Error("upload error");
+        };
+
+        // Execute
+        this.cut.uploadButton.click();
+        await new Promise<void>((resolve) =>
+          this.cut.once("updateError", resolve)
+        );
+
+        // Verify
+        await asyncAssertScreenshot(
+          path.join(__dirname, "/update_avatar_page_upload_error.png"),
+          path.join(__dirname, "/golden/update_avatar_page_upload_error.png"),
+          path.join(__dirname, "/update_avatar_page_upload_error_diff.png")
+        );
+
+        // Prepare
+        clientMock.send = async (request) => {
+          let toBeSent = await new Promise<string>((resolve) => {
+            let fileReader = new FileReader();
+            fileReader.onload = () => {
+              resolve(fileReader.result as string);
+            };
+            fileReader.readAsBinaryString(request.body as Blob);
+          });
+          await writeFile(
+            path.join(__dirname, "/update_avatar_page_uploaded_image.png"),
+            toBeSent
+          );
+          return {} as UploadAvatarResponse;
+        };
+
+        // Execute
+        this.cut.uploadButton.click();
+        await new Promise<void>((resolve) => this.cut.once("updated", resolve));
+
+        // Verify
+        await asyncAssertImage(
+          path.join(__dirname, "/update_avatar_page_uploaded_image.png"),
+          path.join(__dirname, "/golden/update_avatar_page_uploaded_image.png"),
+          path.join(__dirname, "/update_avatar_page_uploaded_image_diff.png")
+        );
+      }
+      public tearDown() {
+        this.cut.remove();
+      }
+    })(),
+    {
+      name: "Back",
+      execute: () => {
+        // Prepare
+        let cut = new UpdateAvatarPage(undefined);
+        let isBack = false;
+        cut.on("back", () => (isBack = true));
+
+        // Execute
+        cut.backMenuItem.click();
+
+        // Verify
+        assertThat(isBack, eq(true), "Back");
+      },
+    },
+  ],
+});
