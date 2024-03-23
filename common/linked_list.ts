fb83a2758444fe1ff593de1b6f8165ebcc853d2a
Yuxuan Xie <teststaybaka@gmail.com>
2024-03-23 05:39:54 +0000

    Add LinkedList.

---
+++ common/linked_list.ts
@@ -0,0 +1,90 @@
+export class LinkedNode<T> {
+  public prev: LinkedNode<T>;
+  public next: LinkedNode<T>;
+
+  public constructor(public reduceSize: () => void, public value?: T) {}
+
+  public remove(): void {
+    let next = this.next;
+    let prev = this.prev;
+    prev.next = next;
+    next.prev = prev;
+    this.reduceSize();
+  }
+}
+
+export class LinkedList<T> {
+  private size = 0;
+  private reduceSize = (): void => {
+    this.size--;
+  };
+  private start = new LinkedNode<T>(this.reduceSize);
+  private end = new LinkedNode<T>(this.reduceSize);
+
+  public constructor() {
+    this.clear();
+  }
+
+  public pushBack(value: T): LinkedNode<T> {
+    let node = new LinkedNode(this.reduceSize, value);
+    let prev = this.end.prev;
+    prev.next = node;
+    node.prev = prev;
+    this.end.prev = node;
+    node.next = this.end;
+    this.size++;
+    return node;
+  }
+
+  public popFront(): T {
+    let ret = this.start.next;
+    ret.remove();
+    return ret.value;
+  }
+
+  public clear(): void {
+    this.start.next = this.end;
+    this.end.prev = this.start;
+    this.size = 0;
+  }
+
+  public getSize(): number {
+    return this.size;
+  }
+
+  public forEach(callback: (arg: T) => void): void {
+    for (let it = this.start.next; it !== this.end; it = it.next) {
+      callback(it.value);
+    }
+  }
+
+  public forEachReverse(callback: (arg: T) => void): void {
+    for (let it = this.end.prev; it !== this.start; it = it.prev) {
+      callback(it.value);
+    }
+  }
+
+  public forEachNode(callback: (arg: LinkedNode<T>) => void): void {
+    for (let it = this.start.next; it !== this.end; ) {
+      let current = it;
+      it = it.next;
+      callback(current);
+    }
+  }
+
+  public forEachNodeReverse(callback: (arg: LinkedNode<T>) => void): void {
+    for (let it = this.end.prev; it !== this.start; ) {
+      let current = it;
+      it = it.prev;
+      callback(current);
+    }
+  }
+
+  public toArray(): T[] {
+    let ret: T[] = [];
+    this.forEach((element): void => {
+      ret.push(element);
+    });
+    return ret;
+  }
+}
---
+++ common/linked_list_test.ts
@@ -0,0 +1,165 @@
+import { LinkedList } from "./linked_list";
+import { TEST_RUNNER } from "@selfage/puppeteer_test_runner";
+import { assertThat, eq, eqArray } from "@selfage/test_matcher";
+
+TEST_RUNNER.run({
+  name: "LinkedListTest",
+  cases: [
+    {
+      name: "PushBackAndClear",
+      execute: () => {
+        // Execute
+        let linkedList = new LinkedList<number>();
+
+        // Verify
+        assertThat(linkedList.toArray(), eqArray([]), "empty");
+
+        // Execute
+        let node = linkedList.pushBack(12311);
+
+        // Verify
+        assertThat(linkedList.toArray(), eqArray([eq(12311)]), "one element");
+        assertThat(node.value, eq(12311), "1st node value");
+
+        // Execute
+        let node2 = linkedList.pushBack(4332);
+
+        // Verify
+        assertThat(
+          linkedList.toArray(),
+          eqArray([eq(12311), eq(4332)]),
+          "two elements"
+        );
+        assertThat(node2.value, eq(4332), "2nd node value");
+      },
+    },
+    {
+      name: "ForEachLoop",
+      execute: () => {
+        // Prepare
+        let linkedList = new LinkedList<number>();
+        linkedList.pushBack(132);
+        linkedList.pushBack(31);
+        linkedList.pushBack(22);
+
+        // Execute
+        let res = new Array<number>();
+        linkedList.forEach((value) => {
+          res.push(value);
+        });
+
+        // Verify
+        assertThat(res, eqArray([eq(132), eq(31), eq(22)]), "forEach");
+      },
+    },
+    {
+      name: "ForEachReverseLoop",
+      execute: () => {
+        // Prepare
+        let linkedList = new LinkedList<number>();
+        linkedList.pushBack(132);
+        linkedList.pushBack(31);
+        linkedList.pushBack(22);
+
+        // Execute
+        let res = new Array<number>();
+        linkedList.forEachReverse((value) => {
+          res.push(value);
+        });
+
+        // Verify
+        assertThat(res, eqArray([eq(22), eq(31), eq(132)]), "forEachReverse");
+      },
+    },
+    {
+      name: "ForEachNodeLoop",
+      execute: () => {
+        // Prepare
+        let linkedList = new LinkedList<number>();
+        linkedList.pushBack(132);
+        linkedList.pushBack(31);
+        linkedList.pushBack(22);
+
+        // Execute
+        let res = new Array<number>();
+        linkedList.forEachNode((node) => {
+          res.push(node.value);
+        });
+
+        // Verify
+        assertThat(res, eqArray([eq(132), eq(31), eq(22)]), "forEachNode");
+      },
+    },
+    {
+      name: "ForEachNodeReverseLoop",
+      execute: () => {
+        // Prepare
+        let linkedList = new LinkedList<number>();
+        linkedList.pushBack(132);
+        linkedList.pushBack(31);
+        linkedList.pushBack(22);
+
+        // Execute
+        let res = new Array<number>();
+        linkedList.forEachNodeReverse((node) => {
+          res.push(node.value);
+        });
+
+        // Verify
+        assertThat(
+          res,
+          eqArray([eq(22), eq(31), eq(132)]),
+          "forEachNodeReverse"
+        );
+      },
+    },
+    {
+      name: "PopFront",
+      execute: () => {
+        // Prepare
+        let linkedList = new LinkedList<number>();
+        linkedList.pushBack(132);
+        linkedList.pushBack(31);
+        linkedList.pushBack(22);
+
+        // Execute
+        let value = linkedList.popFront();
+
+        // Verify
+        assertThat(value, eq(132), "popFront");
+      },
+    },
+    {
+      name: "NodeRemove",
+      execute: () => {
+        // Prepare
+        let linkedList = new LinkedList<number>();
+        linkedList.pushBack(132);
+        let node = linkedList.pushBack(31);
+        linkedList.pushBack(22);
+
+        // Execute
+        node.remove();
+
+        // Verify
+        assertThat(linkedList.toArray(), eqArray([eq(132), eq(22)]), "removed");
+      },
+    },
+    {
+      name: "Clear",
+      execute: () => {
+        // Prepare
+        let linkedList = new LinkedList<number>();
+        linkedList.pushBack(132);
+        linkedList.pushBack(31);
+        linkedList.pushBack(22);
+
+        // Execute
+        linkedList.clear();
+
+        // Verify
+        assertThat(linkedList.toArray(), eqArray([]), "cleared");
+      },
+    },
+  ],
+});
