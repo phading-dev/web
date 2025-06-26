0c992acbb0a015bd3b8c835b5999fc2ee213c195
Ykuyo <ykuyo.dev@gmail.com>
2025-06-26 12:46:20 -0700

    Tweaked default speed for danmaku. Try fullscreen from document.body.

--- main_app/consumer_page/play_page/body.ts
+++ main_app/consumer_page/play_page/body.ts
@@ -662,7 +662,7 @@ export class PlayPage extends EventEmitter {
 
   private goFullscreen(): void {
     if (!document.fullscreenElement) {
-      this.body.requestFullscreen();
+      document.body.requestFullscreen();
     }
   }
 
