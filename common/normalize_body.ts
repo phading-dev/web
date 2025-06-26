db3b36b2a7a0107679fbada2928c247c6a402b5f
Ykuyo <ykuyo.dev@gmail.com>
2025-06-26 12:51:15 -0700

    Try adding overflow: auto to document.body for fullscreen scrolling.

--- common/normalize_body.ts
+++ common/normalize_body.ts
@@ -15,6 +15,7 @@ export function normalizeBody() {
   document.body.style.fontFamily = "sans-serif";
   document.body.style.width = "100%";
   document.body.style.height = "100%";
+  document.body.style.overflow = "auto";
 
   window.removeEventListener("resize", setRootFontSize);
   setRootFontSize();
