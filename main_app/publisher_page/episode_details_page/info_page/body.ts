53938ab7aaefc2dfa2727a9be96e3fcc96f6859a
Ykuyo <ykuyo.dev@gmail.com>
2025-07-27 11:38:23 -0700

    Try debug hls playback.

--- main_app/publisher_page/episode_details_page/info_page/body.ts
+++ main_app/publisher_page/episode_details_page/info_page/body.ts
@@ -246,7 +246,9 @@ export class InfoPage extends EventEmitter {
       style: `margin-top: 2rem; width: 100%; object-fit: contain;`,
       controls: "true",
     });
-    this.hls = new Hls();
+    this.hls = new Hls({
+      debug: true,
+    });
     this.hls.loadSource(videoUrl);
     this.hls.attachMedia(this.video);
     return [this.video];
